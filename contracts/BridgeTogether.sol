822324567e725b9052af90042bda30945c8b8a9c
Jarrett Kong <jarrett.kong@coinbase.com>
2022-02-01 16:28:22 -0500

    adjustments to bridge contract

--- contracts/BridgeTogether.sol
+++ contracts/BridgeTogether.sol
@@ -1,16 +1,29 @@
 pragma solidity 0.8.11;
 
 contract BridgeTogether {
-    address payable public owner;
     mapping(address => uint256) balances;
-    mapping(address => uint256) l2balances;
 
-    constructor() {
-        owner = payable(msg.sender);
-    }
+    event LogDeposit(address accountAddress, uint256 amount);
+    event LogWithdraw(address accountAddress, uint256 amount);
+    event LogBalance(address accountAddress, uint256 amount);
 
     function deposit() public payable {
         balances[msg.sender] += msg.value;
+        emit LogDeposit(msg.sender, msg.value);
+        logBalance();
+    }
+
+    function withdraw() public payable {
+        require((balances[msg.sender] - msg.value) >= 0); // can't withdraw more than their balance
+
+        balances[msg.sender] -= msg.value;
+        emit LogWithdraw(msg.sender, msg.value);
+        logBalance();
+    }
+
+    function logBalance() internal {
+        uint256 balance = getBalance();
+        emit LogBalance(msg.sender, balance);
     }
 
     function getBalance() public view returns (uint256) {
